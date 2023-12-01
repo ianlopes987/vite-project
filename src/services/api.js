78631202990be4490a826ab28d01a41f5b18a14b
sebastiao.lopes <lopescastroneto@gmail.com>
2023-12-01 18:38:08 -0300

    token add

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ export const Api = axios.create({
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBzLWx1a2UtZG90LWF1dG9hdmFsaWFyLWFwcHMuYXBwc3BvdC5jb20iLCJpYXQiOjE3MDEzODQzNDIsImp0aSI6IjgyN2Y3YzJhM2NhOWQzNjllYjQ0OGUyMzE3Yjg1ZjEyNzQ4NDJhMGMiLCJuYmYiOjE3MDEzODQzNDIsImV4cCI6MTcwMTQ3MDc0MiwiZGF0YSI6eyJjb3VudHJ5X2lkIjoiNzYiLCJpbnN0YW5jZV9pZCI6MTMyNDkxLCJ0b2tlbl9pZCI6MTU1NTk2NDQ0LCJ0eXBlIjoiYXV0aG9yaXphdGlvbiJ9fQ.QMrRat4D2IYXrX3Dy73RtExq-6-XUlkoBaeb6WxKMiA",
+        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBzLWx1a2UtZG90LWF1dG9hdmFsaWFyLWFwcHMuYXBwc3BvdC5jb20iLCJpYXQiOjE3MDE0NjY2MzcsImp0aSI6IjgyN2Y3YzJhM2NhOWQzNjllYjQ0OGUyMzE3Yjg1ZjEyNzQ4NDJhMGMiLCJuYmYiOjE3MDE0NjY2MzcsImV4cCI6MTcwMTU1MzAzNywiZGF0YSI6eyJjb3VudHJ5X2lkIjoiNzYiLCJpbnN0YW5jZV9pZCI6MTMyNDkxLCJ0b2tlbl9pZCI6MTU1Njk2NDUwLCJ0eXBlIjoiYXV0aG9yaXphdGlvbiJ9fQ.RoRAuviHUZQpmS70aHZb78TAlNPn-RGVbCMJrfqEviE",
          "signature": "9587915e-367335b4-e286dbc4-35585857-db78c0c5"
     }
 

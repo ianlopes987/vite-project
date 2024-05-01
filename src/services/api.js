52bc350bd45bdec2c2d8c9260bbe287555884ce9
Sebastiao Lopes <55919047+sebastiaoLopess@users.noreply.github.com>
2024-05-01 10:30:23 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -8,7 +8,7 @@ export const Api = axios.create({
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBzLWx1a2UtZG90LWF1dG9hdmFsaWFyLWFwcHMuYXBwc3BvdC5jb20iLCJpYXQiOjE3MTQ0ODc0OTMsImp0aSI6IjgyN2Y3YzJhM2NhOWQzNjllYjQ0OGUyMzE3Yjg1ZjEyNzQ4NDJhMGMiLCJuYmYiOjE3MTQ0ODc0OTMsImV4cCI6MTcxNDU3Mzg5MywiZGF0YSI6eyJjb3VudHJ5X2lkIjoiNzYiLCJpbnN0YW5jZV9pZCI6MTMyNDkxLCJ0b2tlbl9pZCI6MTc1NDUxOTU5LCJ0eXBlIjoiYXV0aG9yaXphdGlvbiJ9fQ.xQx_Zq32E6AoSUD_roe5-2NGvr7g_OjDr6My6ja5qpU",
+        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBzLWx1a2UtZG90LWF1dG9hdmFsaWFyLWFwcHMuYXBwc3BvdC5jb20iLCJpYXQiOjE3MTQ1NzAxNTQsImp0aSI6IjgyN2Y3YzJhM2NhOWQzNjllYjQ0OGUyMzE3Yjg1ZjEyNzQ4NDJhMGMiLCJuYmYiOjE3MTQ1NzAxNTQsImV4cCI6MTcxNDY1NjU1NCwiZGF0YSI6eyJjb3VudHJ5X2lkIjoiNzYiLCJpbnN0YW5jZV9pZCI6MTMyNDkxLCJ0b2tlbl9pZCI6MTc1NTc0MzM3LCJ0eXBlIjoiYXV0aG9yaXphdGlvbiJ9fQ.gB_GA1Iceu0VOZ3sr5lZ0aULj3D-ZSh0zHOoLLlPch4",
          "signature": "9587915e-367335b4-e286dbc4-35585857-db78c0c5"
     }
 

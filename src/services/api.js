5dac5a8dbf8b3d40379ab1a56de15f1ba2f84fee
Matheus <81726876+mthSolon@users.noreply.github.com>
2024-07-04 18:53:04 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -8,7 +8,7 @@ export const Api = axios.create({
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBzLWx1a2UtZG90LWF1dG9hdmFsaWFyLWFwcHMuYXBwc3BvdC5jb20iLCJpYXQiOjE3MjAwNDM2MTIsImp0aSI6IjgyN2Y3YzJhM2NhOWQzNjllYjQ0OGUyMzE3Yjg1ZjEyNzQ4NDJhMGMiLCJuYmYiOjE3MjAwNDM2MTIsImV4cCI6MTcyMDEzMDAxMiwiZGF0YSI6eyJjb3VudHJ5X2lkIjoiNzYiLCJpbnN0YW5jZV9pZCI6IjEzMjQ5MSIsInRva2VuX2lkIjoxODQ0NDAxNjMsInR5cGUiOiJhdXRob3JpemF0aW9uIn19.lN7mwzgOiiUigOuEX8LjqQei1Rn-OzstvJQ7jWunRVk",
+        "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHBzLWx1a2UtZG90LWF1dG9hdmFsaWFyLWFwcHMuYXBwc3BvdC5jb20iLCJpYXQiOjE3MjAxMjk5NjksImp0aSI6IjgyN2Y3YzJhM2NhOWQzNjllYjQ0OGUyMzE3Yjg1ZjEyNzQ4NDJhMGMiLCJuYmYiOjE3MjAxMjk5NjksImV4cCI6MTcyMDIxNjM2OSwiZGF0YSI6eyJjb3VudHJ5X2lkIjoiNzYiLCJpbnN0YW5jZV9pZCI6IjEzMjQ5MSIsInRva2VuX2lkIjoxODQ1ODk0ODcsInR5cGUiOiJhdXRob3JpemF0aW9uIn19.GfvzlKIseflL0BBLWuZAF1Oohqa6slVGu8ijgMYtRTg",
          "signature": "9587915e-367335b4-e286dbc4-35585857-db78c0c5"
     }
 
